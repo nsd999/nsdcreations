9eb29418182063e92f8cec4a0456b87997b57a13
Sai Dheeraj Nalkari <nalkarisaidheeraj@gmail.com>
2026-09-25 13:52:03 +0530

    fix: make dark theme the default and prevent system override

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -7,8 +7,8 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem={true}
+      defaultTheme="dark"
+      enableSystem={false}
       disableTransitionOnChange
     >
       {children}
